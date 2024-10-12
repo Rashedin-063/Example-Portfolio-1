fa6d8f828a62961786f39799ea406d69db799a43
RU063 <rashedinislam.06@gmail.com>
2024-10-12 23:53:18 +0600

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="text-center">
+
+      <h4 className="text-green-600 font-3xl">My Awesome Portfolio</h4>
+    </div>
+  )
+}
+export default Home
