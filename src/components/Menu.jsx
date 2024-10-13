60c9f335a7f7e94c37e0eaddffd601738feb4f61
RU063 <rashedinislam.06@gmail.com>
2024-10-13 10:56:29 +0600

    logo and custom font added

---
+++ src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Fade } from 'react-awesome-reveal';
+
+const Menu = ({ filteredItems = [] }) => {  
+
+  return (
+    <Fade cascade damping={0.3}>
+      <ul className='xl:flex xl:items-center xl:gap-3 xl:text-lg space-y-4 xl:space-y-0 font-wendy'>
+       
+        {filteredItems.map(({ to, label }) => (
+          <li key={to}>
+            <NavLink
+              to={to}
+              className={({ isActive }) =>
+                isActive
+                  ? ' text-green-lantern border-2 rounded-lg border-deep-ocean px-3 py-2 xl:text-lg'
+                  : 'px-3 hover:border-b-2 hover:rounded-xl border-green-lantern text-deep-ocean hover:transition text-base'
+              }
+            >
+              {label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </Fade>
+  );
+};
+
+Menu.propTypes = {
+  items: PropTypes.array,
+}
+
+export default Menu;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      playWrite: "'Playwrite GB S', cursive",
+      chakraPatch: "'Chakra Petch', sans-serif",
+      josefin: "'Josefin Sans', sans-serif",
+      sevillana: "'Sevillana', cursive",
+    },
+    extend: {
+      colors: {
+        'faded-pearl': '#E6DFAF',
+        'midnight-gray': '	##3c3c3c',
+        'deep-ocean': '#004080',
+        'green-lantern': '#004d25',
+        'pure-white': '#FFFFFF',
+        'charcoal-gray': '#000000',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
